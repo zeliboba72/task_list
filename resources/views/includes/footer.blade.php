f5105cd7975b060b29e962d2e8531b52bd7592b2
Evgeny Smagin <snailman7297@gmail.com>
2021-11-20 15:31:30 +0500

    created base layout

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="border-top border-2">
+    <div class="container pt-2 pb-2">
+        <p class="fs-6 text-secondary text-center m-0">SMG, {{ date('Y') }}</p>
+    </div>
+</footer>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,5 @@
+<header class="bg-primary">
+    <div class="container pt-1 pb-1">
+        <a href="" class="fs-3 text-light text-decoration-none">{{ env('APP_NAME') }}</a>
+    </div>
+</header>
