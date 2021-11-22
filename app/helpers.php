49e82e4781eb36b8aa03eb6fe6cc4d3c37d04734
Evgeny Smagin <snailman7297@gmail.com>
2021-11-22 21:38:56 +0500

    add helper function for get true users

--- app/helpers.php
+++ app/helpers.php
@@ -1,4 +1,6 @@
 <?php
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 if (!function_exists('checkDateFormat')) {
     function checkDateFormat($date) {
@@ -10,5 +12,12 @@ if (!function_exists('checkDateFormat')) {
         $month = $arDate[1];
         $day = $arDate[2];
         return checkdate($month, $day, $year);
+    }
+}
+
+if (!function_exists('getSubordinatesForCurrentUser')) {
+    function getSubordinatesForCurrentUser() {
+        $currentId = Auth::user()->id;
+        return User::where('supervisor', '=', $currentId)->get();
     }
 }
