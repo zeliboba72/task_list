f5aea3050cee67fcf6233f50da73bd226aa4b74d
Evgeny Smagin <snailman7297@gmail.com>
2021-11-23 22:20:06 +0500

    add new methods for task-controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,7 +10,7 @@ class TaskController extends Controller
 {
     public function index(Request $request)
     {
-        $users = User::all();
+        $users = getSubordinatesForCurrentUser();
         $filters = collect();
 
         if ($request->get('date')) {
@@ -51,5 +51,76 @@ class TaskController extends Controller
         $tasks = $tasksQuery->get();
 
         return view('index', compact('tasks', 'users', 'filters'));
+    }
+
+    public function create()
+    {
+        $users = User::where('supervisor', '=', auth()->user()->id)->get();
+        return view('create', compact('users'));
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            "title"              => "required|min:6|max:255",
+            "description"        => "required|min:6|max:999",
+            "execution"          => "required|",
+            "expiration_date"    => "required|date",
+            "responsible_person" => "required",
+            "creator"            => "required",
+        ]);
+
+        $task = new Task();
+        $task->fill($data);
+        $task->save();
+
+        return redirect()->route('tasks.index');
+    }
+
+    public function edit($id)
+    {
+        $task = Task::findOrFail($id);
+
+        // Проверяем, является ли пользователем создателем или исполнителем задачи
+        if ($task->creator != auth()->user()->id && $task->responsible_person != auth()->user()->id) {
+            return redirect()->route('tasks.index');
+        }
+
+        // Проверяем, если задача создана не самим пользователем, а его руководителем
+        if ($task->creator == auth()->user()->supervisor) {
+            return view('edit-only-status', compact('task'));
+        }
+
+        $users = User::where('supervisor', '=', auth()->user()->id)->get();
+        return view('edit', compact('users', 'task'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $task = Task::findOrFail($id);
+
+        // Проверяем, является ли пользователем создателем или исполнителем задачи
+        if ($task->creator != auth()->user()->id && $task->responsible_person != auth()->user()->id) {
+            return redirect()->route('tasks.index');
+        }
+
+        if ($task->creator == auth()->user()->supervisor) {
+            $data = $request->validate([
+                "status" => "required",
+            ]);
+        } else {
+            $data = $request->validate([
+                "title"              => "required|min:6|max:255",
+                "description"        => "required|min:6|max:999",
+                "execution"          => "required",
+                "expiration_date"    => "required|date",
+                "responsible_person" => "required",
+                "status"             => "required",
+            ]);
+        }
+
+        $task->fill($data);
+        $task->save();
+        return redirect()->route('tasks.index');
     }
 }
